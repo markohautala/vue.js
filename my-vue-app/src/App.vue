286fa863058b0f38338f822f25aacde4b9bd27c1
markohautala <markohautala@live.se>
2025-04-24 14:35:15 +0200

    add Firebase integration and navigation links

--- my-vue-app/src/App.vue
+++ my-vue-app/src/App.vue
@@ -2,6 +2,12 @@
 </script>
 
 <template>
+  <nav>
+    <router-link to="/"> Home </router-link>
+    <router-link to="/feed"> Feed </router-link>
+    <router-link to="/register"> Register </router-link>
+    <router-link to="/sign-in"> Login </router-link>
+  </nav>
   <router-view />
 </template>
 
--- my-vue-app/src/main.js
+++ my-vue-app/src/main.js
@@ -3,6 +3,24 @@ import './style.css'
 import App from './App.vue'
 import router from './router'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC2-ivyvDenKmKEGkXBfmZjFPSYC-ZvP3A",
+  authDomain: "vue-firebase-auth-7c95f.firebaseapp.com",
+  projectId: "vue-firebase-auth-7c95f",
+  storageBucket: "vue-firebase-auth-7c95f.firebasestorage.app",
+  messagingSenderId: "914367416383",
+  appId: "1:914367416383:web:cceb179fc4cb0ab05fd08a"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
 const app = createApp(App)
 
 app.use(router)
