c688521581c4b72368adc2e3b96b1e5efab38283
markohautala <markohautala@live.se>
2025-04-27 22:40:35 +0200

    add registration form to Register page

--- my-vue-app/src/views/Register.vue
+++ my-vue-app/src/views/Register.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
-    <h1>Register</h1>
     <p>Welcome to the Register page!</p>
+    <p>Please fill in the form below to create an account.</p>
+    <p>Type your email here <input type="text" placeholder="Email" v-model="email"/></p>
+    <p>Type your password here <input type="password" placeholder="Password" v-model="password"/></p>
+    <p><button @click="register">Submit</button></p>
+    <p><button @click="signInWithGoogle">Sign In With Google</button></p>
   </div>
 </template>
 
