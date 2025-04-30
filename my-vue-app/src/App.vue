a57a8addc1044e7903d7281203da1c2507cb22e3
markohautala <markohautala@live.se>
2025-04-30 18:04:44 +0200

    add authentication state management and sign-out functionality in App.vue

--- my-vue-app/src/App.vue
+++ my-vue-app/src/App.vue
@@ -7,9 +7,40 @@
     <router-link to="/feed"> Feed </router-link>
     <router-link to="/register"> Register </router-link>
     <router-link to="/sign-in"> Login </router-link>
+    <button @click="handleSignOut" v-if="isLoggedIn">Sign Out</button>
   </nav>
   <router-view />
 </template>
+
+<script setup>
+import { onMounted, ref } from 'vue';
+import { getAuth, onAuthStateChanged, signOut } from 'firebase/auth';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const isLoggedIn = ref(false);
+
+const handleSignOut = () => {
+  signOut(auth).then(() => {
+    router.push('/sign-in');
+  }).catch((error) => {
+    console.error('Sign out error:', error);
+  });
+};
+
+const auth = getAuth();
+onMounted(() => {
+  auth = getAuth();
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      isLoggedIn.value = true;
+    } else {
+      isLoggedIn.value = false;
+    }
+  })
+})
+
+</script>
 
 <style scoped>
 .logo {
