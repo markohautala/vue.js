0d4291d5bd74969dd3a528fbfd07704733b20a82
markohautala <markohautala@live.se>
2025-05-01 15:33:28 +0200

    refactor Register.vue for improved structure and enhanced user experience

--- my-vue-app/src/views/Register.vue
+++ my-vue-app/src/views/Register.vue
@@ -3,67 +3,94 @@
     <h3>Register</h3>
     <p>Please fill in the form below to create an account.</p>
     <p>
-      <input id="email" type="text" placeholder="Email" v-model="email" style="width: 80%; height: 40px; font-size: 16px;" />
+      <input
+        id="email"
+        type="text"
+        placeholder="Email"
+        v-model="email"
+        style="width: 80%; height: 40px; font-size: 16px"
+      />
     </p>
     <p>
-      <input id="password" type="password" placeholder="Password" v-model="password" @keyup.enter="signIn" style="width: 80%; height: 40px; font-size: 16px;" />
+      <input
+        id="password"
+        type="password"
+        placeholder="Password"
+        v-model="password"
+        @keyup.enter="register"
+        style="width: 80%; height: 40px; font-size: 16px"
+      />
     </p>
     <p><button @click="register">Register</button></p>
     <p>Or</p>
-    <p><button @click="signInWithGoogle"><font-awesome-icon :icon="['fab', 'google']" /> Sign In With Google</button></p>
-    <p><button @click="signInWithGithub"><font-awesome-icon :icon="['fab', 'github']" /> Sign In With Github</button></p>
-    <p>Already have an account? <router-link to="/sign-in">Login</router-link></p>
+    <p>
+      <button @click="signInWithGoogle">
+        <font-awesome-icon :icon="['fab', 'google']" /> Sign In With Google
+      </button>
+    </p>
+    <p>
+      <button @click="signInWithGithub">
+        <font-awesome-icon :icon="['fab', 'github']" /> Sign In With Github
+      </button>
+    </p>
+    <p>
+      Already have an account? <router-link to="/sign-in">Login</router-link>
+    </p>
   </div>
 </template>
 
 <script setup>
-  import { ref } from 'vue';
-  import { useRouter } from 'vue-router';
-  import { getAuth, createUserWithEmailAndPassword, GoogleAuthProvider, GithubAuthProvider, signInWithPopup } from 'firebase/auth';
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  GithubAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
 
-  const email = ref('');
-  const password = ref('');
-  const Router = useRouter();
+const email = ref("");
+const password = ref("");
+const Router = useRouter();
 
-  const register = () => {
-    createUserWithEmailAndPassword(getAuth(), email.value, password.value)
-      .then((data) => {
-        console.log("User registered successfully");
-        Router.push('/feed');
-      })
-      .catch((error) => {
-        console.log(error.code);
-        alert(error.message);
-      });
-  };
+const register = () => {
+  createUserWithEmailAndPassword(getAuth(), email.value, password.value)
+    .then((data) => {
+      console.log("User registered successfully");
+      Router.push("/feed");
+    })
+    .catch((error) => {
+      console.log(error.code);
+      alert(error.message);
+    });
+};
 
-  const signInWithGoogle = () => {
-    const provider = new GoogleAuthProvider();
-    signInWithPopup(getAuth(), provider)
-      .then((result) => {
-        console.log("User signed in with Google successfully");
-        Router.push('/feed');
-      })
-      .catch((error) => {
-        console.log(error.code);
-        alert(error.message);
-      });
-  };
+const signInWithGoogle = () => {
+  const provider = new GoogleAuthProvider();
+  signInWithPopup(getAuth(), provider)
+    .then((result) => {
+      console.log("User signed in with Google successfully");
+      Router.push("/feed");
+    })
+    .catch((error) => {
+      console.log(error.code);
+      alert(error.message);
+    });
+};
 
-  const signInWithGithub = () => {
-    const provider = new GithubAuthProvider();
-    signInWithPopup(getAuth(), provider)
-      .then((result) => {
-        console.log("User signed in with Github successfully");
-        Router.push('/feed');
-      })
-      .catch((error) => {
-        console.log(error.code);
-        alert(error.message);
-      });
-  };
-
-
+const signInWithGithub = () => {
+  const provider = new GithubAuthProvider();
+  signInWithPopup(getAuth(), provider)
+    .then((result) => {
+      console.log("User signed in with Github successfully");
+      Router.push("/feed");
+    })
+    .catch((error) => {
+      console.log(error.code);
+      alert(error.message);
+    });
+};
 </script>
 
 <style scoped>
@@ -77,5 +104,4 @@ input {
   border: 2px solid #230000;
   border-radius: 15px;
 }
-
 </style>
